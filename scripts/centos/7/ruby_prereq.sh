277640fffed44b96c27785e3a03564b7161185b8
Eric Shamow <alice@example.com>
2016-06-01 13:21:56 -0700

    Working Ruby 2.3.1

--- scripts/centos/7/ruby_prereq.sh
+++ scripts/centos/7/ruby_prereq.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-export PATH=/opt/puppetlabs/bin:$PATH
-
-puppet module install covermymeds-ruby
-yum -y install scl-utils
-rpm -ivh https://www.softwarecollections.org/en/scls/rhscl/rh-ruby22/epel-7-x86_64/download/rhscl-rh-ruby22-epel-7-x86_64.noarch.rpm
-yum clean all
+sudo yum -y install rpmdevtools
+sudo yum -y install glibc-devel readline-devel libyaml-devel ncurses-devel gdbm-devel tcl-devel openssl-devel compat-db47 libffi-devel
+sudo yum -y install make gcc unzip byacc
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cd ~/rpmbuild
+wget https://github.com/slalompdx/ruby/raw/master/ruby.spec -O ~/rpmbuild/SPECS/ruby.spec
+wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz -O ~/rpmbuild/SOURCES/ruby-2.3.1.tar.gz
+rpmbuild -ba ~/rpmbuild/SPECS/ruby.spec
+sudo yum -y install ~/rpmbuild/RPMS/x86_64/ruby2-2.3.1-1.el7.centos.x86_64.rpm
