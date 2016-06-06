4452aa1835fc67b4633bdbed4c69c938e0c17708
Eric Shamow <alice@example.com>
2016-06-06 16:13:40 -0700

    Working puppet4 base

--- puppet/ruby/ruby.pp
+++ puppet/ruby/ruby.pp
@@ -1,9 +1,66 @@
-class { 'ruby':
-  rubies => ['rh-ruby22'],
+class { '::wget': }
+$orig_packageset =
+  [
+    'rpmdevtools',
+    'glibc-devel',
+    'readline-devel',
+    'libyaml-devel',
+    'ncurses-devel',
+    'gdbm-devel',
+    'tcl-devel',
+    'openssl-devel',
+    'libffi-devel',
+    'make',
+    'gcc',
+    'unzip',
+    'byacc'
+  ]
+
+if $facts['os']['release']['major'] == 7 {
+  $packageset = $orig_packageset + 'compat-db47'
 }
-ruby::gems { 'rh-ruby22':
-  gems => { 'bundler'    => { 'version' => '1.11.2' },
-            'rake'       => { 'version' => '11.1.2' },
-            'serverspec' => { 'version' => '2.33.0' }
-          }
+else {
+  $packageset = $orig_packageset + 'db4-devel'
+}
+
+package { $orig_packageset: } ->
+
+file {
+  [
+    '/home/vagrant/rpmbuild',
+    '/home/vagrant/rpmbuild/BUILD',
+    '/home/vagrant/rpmbuild/RPMS',
+    '/home/vagrant/rpmbuild/SOURCES',
+    '/home/vagrant/rpmbuild/SPECS',
+    '/home/vagrant/rpmbuild/SRPMS'
+  ]:
+    ensure => directory,
+    owner  => 'vagrant',
+    group  => 'vagrant',
+} ->
+
+wget::fetch { 'Download spec file':
+  source => "https://github.com/slalompdx/ruby/raw/master/ruby-el${facts['os']['release']['major']}.spec",
+  destination => '/home/vagrant/rpmbuild/SPECS/ruby.spec',
+} ->
+
+wget::fetch { 'Download Ruby bundle':
+  source => 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz',
+  destination => '/home/vagrant/rpmbuild/SOURCES/ruby-2.3.1.tar.gz',
+} ->
+
+exec { 'build ruby rpm':
+  command => '/usr/bin/rpmbuild -ba --define "_topdir /home/vagrant/rpmbuild" /home/vagrant/rpmbuild/SPECS/ruby.spec',
+  user    => 'vagrant',
+  cwd     => '/home/vagrant',
+  creates => "/home/vagrant/rpmbuild/RPMS/x86_64/ruby2-2.3.1-1.el${facts['os']['release']['major']}.x86_64.rpm",
+  timeout => 0,
+} ->
+
+exec { 'install ruby rpm':
+  command => "/usr/bin/yum -y localinstall /home/vagrant/rpmbuild/RPMS/x86_64/ruby2-2.3.1-1.el${facts['os']['release']['major']}.x86_64.rpm",
+} ->
+
+exec { 'install minimal gems':
+  command => '/usr/local/bin/gem install bundler rake serverspec',
 }
--- scripts/centos/6/ruby_prereq.sh
+++ scripts/centos/6/ruby_prereq.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-sudo yum -y install rpmdevtools
-sudo yum -y install glibc-devel readline-devel libyaml-devel ncurses-devel gdbm-devel tcl-devel openssl-devel db4-devel libffi-devel
-sudo yum -y install make gcc unzip byacc
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cd ~/rpmbuild
-wget https://github.com/slalompdx/ruby/raw/master/ruby-el6.spec -O ~/rpmbuild/SPECS/ruby.spec
-wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz -O ~/rpmbuild/SOURCES/ruby-2.3.1.tar.gz
-rpmbuild -ba ~/rpmbuild/SPECS/ruby.spec
-sudo yum -y localinstall ~/rpmbuild/RPMS/x86_64/ruby2-2.3.1-1.el6.x86_64.rpm
+ln -s /opt/puppetlabs/bin/puppet /usr/bin/
+puppet module install maestrodev-wget
